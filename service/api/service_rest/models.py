2cf07c1ef276ef1577d68d38408af4a37bd4a151
Anri Astora <anri.astora1001@gmail.com>
2023-01-23 16:22:24 -0500

    models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class AutomobileVO(models.Model):
+    href = models.CharField(max_length=150)
+    vin = models.CharField(max_length=17, unique=True)
+
+    name = models.CharField(max_length=100)
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.PositiveSmallIntegerField(null = True)
+    picture_url = models.URLField()
+
+
+class Technician(models.Model):
+    name = models.CharField(max_length=100)
+
+class Appointment(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+    customer_name = models.CharField(max_length=100)
+    date = models.DateField(null = True)
+    time = models.TimeField(null = True)
+
+    status = models.BooleanField(default=False)
+    
+    technician = models.ForeignKey(Technician, null = True, default=None, on_delete=models.CASCADE)
